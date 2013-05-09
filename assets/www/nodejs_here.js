f379a2a7f5551fa8cb300bd2a72e6a2ddc1d9d12
Warren Falk <wfalk@divisionsinc.com>
2013-05-09 15:40:40 -0400

    More tools to enable rapid development on iphone

--- assets/www/nodejs_here.js
+++ assets/www/nodejs_here.js
@@ -5,10 +5,68 @@ if (typeof WScript !== "undefined") {
 }
 
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello\n');
+	var reqpath = url.parse(req.url).pathname;
+	var seg = reqpath.split('/');
+	
+	if (seg.length <= 1) {
+		console.log('Request for ' + reqpath);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('<a href="/test/">test</a>\n');
+	}
+	else {
+		var root = seg[1];
+		if (root == "test") {
+			reqpath = seg.slice(2).join('/');
+			if (reqpath == '')
+				reqpath = 'index.html';
+			fs.exists(reqpath, function(exists) {
+				if (!exists) {
+					res.writeHeader(404, {"Content-Type": "text/plain"});
+					res.write("404 Not Found\n");
+					res.end();
+				}
+				else {
+					fs.readFile(reqpath, "binary", function(err, file) {
+						if (err) {
+							res.writeHeader(500, {"Content-Type": "text/plain"});
+							res.write(err + "\n");
+							res.end();
+						}
+						else {
+							if (reqpath.endsWith(".html"))
+								contentType = "text/html";
+							else if (reqpath.endsWith(".gif"))
+								contentType = "image/gif";
+							else if (reqpath.endsWith(".svg"))
+								contentType = "image/svg+xml";
+							else if (reqpath.endsWith(".png"))
+								contentType = "image/png";
+							else if (reqpath.endsWith(".css"))
+								contentType = "text/css";
+							else if (reqpath.endsWith(".js"))
+								contentType = "text/javascript";
+							else if (reqpath.endsWith(".xml"))
+								contentType = "text/xml";
+							else
+								contentType = "application/x-octet-stream";
+							res.writeHeader(200, {"Content-Type": contentType});
+							res.write(file, "binary");
+							res.end();
+						}
+					});
+				}
+			});
+		}
+	}
 }).listen(8890, '0.0.0.0');
 
 console.log('Server running http://0.0.0.0:8890/');
