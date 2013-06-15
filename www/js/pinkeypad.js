faff116c9a85a5e0df5206242662636df373aef6
Warren Falk <warren@warrenfalk.com>
2013-06-15 15:31:59 -0400

    add "clear" and "change login" buttons to pin keypad

--- www/js/pinkeypad.js
+++ www/js/pinkeypad.js
@@ -47,26 +47,53 @@ function PinKeyPad(div) {
 
 	this.keyClick = function(key) {
 		var keynum = key.index;
-		control.currentPin = control.currentPin + ('' + keynum);
-		$.each(control.feedbackCells, function(i,v) {
-			v.cell.toggleClass('filled', control.currentPin.length > i);
-		});
-		if (control.currentPin.length == pinLength) {
-			var event = createCustomEvent('enter', { detail: { control: control, pin: this.currentPin }});
+		if (key.index < 10) {
+			// number key was pressed
+			control.currentPin = control.currentPin + ('' + keynum);
+			$.each(control.feedbackCells, function(i,v) {
+				v.cell.toggleClass('filled', control.currentPin.length > i);
+			});
+			if (control.currentPin.length == pinLength) {
+				var event = createCustomEvent('enter', { detail: { control: control, pin: this.currentPin }});
+				div.dispatchEvent(event);
+			}
+		}
+		else if (key.index == 11) {
+			control.currentPin = '';
+			$.each(control.feedbackCells, function(i,v) {
+				v.cell.removeClass('filled');
+			});
+		}
+		else if (key.index == 10) {
+			control.currentPin = '';
+			$.each(control.feedbackCells, function(i,v) {
+				v.cell.removeClass('filled');
+			});
+			var event = createCustomEvent('cancel', { detail: { control: control }});
 			div.dispatchEvent(event);
 		}
 	};
 
-	$.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], function(i,k) {
+	$.each([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "L", "R"], function(i,k) {
 		var $key = $('<button class="key"/>');
 		$key.addClass('key' + k);
 		var $keytext = $('<span class="text"/>');
-		$keytext.text(k);
 		$key.append($keytext);
-		var key = { button: $key, index: k };
+		var key = { button: $key, index: i };
+		$key.mousedown(function() { control.keyClick(key); })
+		switch (k) {
+			case "R":
+				$keytext.text("Clear");
+				break;
+			case "L":
+				$keytext.text("Change Login");
+				break;
+			default:
+				$keytext.text(k);
+				control.keys[k] = key;
+				break;
+		}
 		$keypad.append($key);
-		control.keys[k] = key;
-		$key.on('click', function() { control.keyClick(key); })
 	});
 
 	this.bind = function(propname, target) {
